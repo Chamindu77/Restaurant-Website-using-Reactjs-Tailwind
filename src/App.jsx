b777f977e03e57a1855b59c691ee617e717bc119
chamindu77 <chamindunipun99@gmail.com>
2024-11-03 20:23:50 +0530

    upload task

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import TopList from "./components/TopList/TopList";
+import BgImage from "./assets/2.png";
+import Banner from "./components/Banner/Banner";
+import OurServices from "./components/OurServices/OurServices";
 
-function App() {
-  const [count, setCount] = useState(0)
+const bgStyle = {
+  backgroundImage: `url(${BgImage})`,
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+};
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div style={bgStyle} className="overflow-x-hidden">
+      <div className="min-h-screen bg-white/50 backdrop-blur-3xl">
+        <Navbar />
+        <Hero />
+        <TopList />
+        <Banner />
+        <OurServices />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
